0fceb22d151731fd5f8ec26834047c1caa16ef2b
winux <78425126+Pedro1234-code@users.noreply.github.com>
2023-11-07 19:00:50 -0300

    First code upload

---
+++ DiscordUWP/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
+
+namespace DiscordUWP
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SystemNavigationManager.GetForCurrentView().BackRequested += MainPage_BackRequested;
+            var currentView = SystemNavigationManager.GetForCurrentView();
+            // Set the back button visibility to Visible
+            currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+        }
+
+        private void MainPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (DiscordLoader.CanGoBack)
+            {
+                DiscordLoader.GoBack();
+                e.Handled = true;
+            }
+        }
+    }
+}
